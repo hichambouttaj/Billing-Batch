da041b3657c8e17ae59f1100ac84a71f6dd49370
HichamBouttaj <bouttajhicham@gmail.com>
2023-11-27 23:23:35 +0100

    testing job

--- src/test/java/com/bojji/billingbatch/BillingBatchApplicationTests.java
+++ src/test/java/com/bojji/billingbatch/BillingBatchApplicationTests.java
@@ -1,34 +1,43 @@
 package com.bojji.billingbatch;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+@SpringBatchTest
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class BillingBatchApplicationTests {
     @Autowired
-    private Job job;
+    private JobLauncherTestUtils jobLauncherTestUtils;
     @Autowired
-    private JobLauncher jobLauncher;
-
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+    @BeforeEach
+    void setUp() {
+        this.jobRepositoryTestUtils.removeJobExecutions();
+    }
     @Test
     void testJobExecution(CapturedOutput output) throws Exception {
         // given
         String inputFile = "/some/input/file";
-        JobParameters jobParameters = new JobParametersBuilder()
+        JobParameters jobParameters = this.jobLauncherTestUtils
+                .getUniqueJobParametersBuilder()
                 .addString("input.file", inputFile)
                 .addString("file.format", "csv", false)
                 .toJobParameters();
 
         // when
-        JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
+        JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
 
         // then
         Assertions.assertTrue(output.getOut().contains("processing billing information from file " + inputFile));
